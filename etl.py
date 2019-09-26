343fb92cd8eb478f486a1ed709948426eefe051d
adrianlievano <adrianlievano1993@gmail.com>
2019-09-26 10:46:31 -0700

    Fix: updated SQL queries, ETL pipeline, and jupyter notebook for clarity

--- etl.py
+++ etl.py
@@ -7,37 +7,44 @@ from sql_queries import *
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page']== 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
-    
+    df['ts'] = pd.to_datetime(df['ts'], unit = 'ms')
+    t = df['ts']
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = list([t.dt.hour, t.dt.day, t.dt.week, t.dt.month, t.dt.year, t.dt.weekday_name])
+    column_labels = ('hour', 'day', 'week', 'month','year', 'weekday_name')
+    time_dict = dict()
+
+    for i in range(len(time_data)):
+        time_dict[column_labels[i]] = time_data[i]
+    
+    time_df = pd.DataFrame(time_dict)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
+
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -47,11 +54,17 @@ def process_log_file(cur, filepath):
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        cur.execute(song_select, (row.song, row.artist, row.length))
+        results = cur.fetchone()
+        
+        if results:
+            songid, artistid = results
+        else:
+            songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row.ts ,row.userId, row.level, songid, artistid, row.sessionId, row.location, 
+                         row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
--- sql_queries.py
+++ sql_queries.py
@@ -8,39 +8,44 @@ time_table_drop = """DROP TABLE IF EXISTS time"""
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id int,\
-start_time int, user_id int, level int, song_id varchar, artist_id varchar, session_id int, artist_location varchar, user_agent varchar);""")
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (start_time timestamp, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, artist_location varchar, user_agent varchar);""")
                          
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level int);""")
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, year int, duration numeric);""")
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration numeric);""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar, artist_location varchar, artist_latitude numeric, artist_longitude numeric);""")
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar, artist_name varchar, artist_location varchar, artist_latitude numeric, artist_longitude numeric);""")
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time int, hour int, day int, week int, month int, year int, weekday int);""")
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (hour int, day int, week int, month int, year int, weekday_name varchar);""")
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id,\
-artist_id, session_id, artist_location, user_agent\
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", (1, 2, 3, 4, 'asd', 'asdasd', 7, 'miami', 'xax'))
+songplay_table_insert = ("""INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, artist_location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);""")
 
 user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)\
-VALUES (%s, %s, %s, %s, %s)""", (1, 'Adrian', 'Lievano', 'M', 5))
+VALUES (%s, %s, %s, %s, %s);""")
 
-song_table_insert = ("""INSERT INTO songs (song_id, title, year, duration)\
-VALUES (%s, %s, %s, %s)""", ('asd', 'BOOM', 2019, 40.2))
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration)\
+VALUES (%s, %s, %s, %s, %s);""")
 
-artist_table_insert = ("""INSERT INTO artists (artist_id, artist_location, artist_latitude, artist_longitude)\
-VALUES (%s, %s, %s, %s)""", (1, 'miami', 30.22, 40.111))
+artist_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s);""")
 
-time_table_insert = ("""INSERT INTO time (start_time, hour, day, week, month, year, weekday)\
-VALUES (%s, %s, %s, %s, %s, %s, %s)""", (1, 2, 3, 4, 5, 6, 7))
+time_table_insert = ("""INSERT INTO time (hour, day, week, month, year, weekday_name)\
+VALUES (%s, %s, %s, %s, %s, %s);""")
 
 # FIND SONGS
+#Implement the song_select query in sql_queries.py to find the song ID and artist ID based on the title, artist name, and duration of a song.
 
-song_select = ("""select * from songs""")
+song_select = ("""SELECT songs.song_id, songs.artist_id FROM songs JOIN artists 
+ON songs.artist_id = artists.artist_id 
+WHERE (songs.title = %s AND artists.artist_name = %s AND songs.duration = %s)
+""")
+
+#song_select = ("""SELECT song_id, artists.artist_id FROM songs,    
+       #           artists WHERE (songs.title = %s 
+       #           AND artists.name = %s AND songs.duration = %s)
+       #        """)
 
 # QUERY LISTS
 
